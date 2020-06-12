fc4264b7bb9ea9b01761a61f7b27db86b35bea9f
santiago bianco <santiago.bianco91@gmail.com>
2020-06-12 00:41:49 -0300

    descomento el print que dice cuanto tarda

--- src/modeloscajas/MiMain.java
+++ src/modeloscajas/MiMain.java
@@ -2,35 +2,48 @@ package modeloscajas;
 
 public class MiMain {
 
-	// CONFIGURACION 7
+	// CONFIGURACION ultima
 
 	private static Double TIEMPO_PROC_CAJA = 1.0;
 	private static Double TIEMPO_SETUP = 100.0;
-	private static Double DESTINOS_POR_PASADA = 4.0;
+	private static Double DESTINOS_POR_PASADA = 9.0;
 
 	private static PasadaPorScanner inicializarScanner() {
 
 		// CAMBIAR ESTO SEGUN EL SET DE DATOS
 
 		PasadaPorScanner scanner = new PasadaPorScanner(DESTINOS_POR_PASADA);
-		scanner.agregarDestinoAProcesar(new Destino(10, "A"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "B"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "C"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "D"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "E"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "F"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "G"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "H"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "I"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "J"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "K"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "L"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "M"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "N"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "O"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "P"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "Q"));
-		scanner.agregarDestinoAProcesar(new Destino(10, "R"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "A"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "B"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "C"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "D"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "E"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "F"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "G"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "H"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "I"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "J"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "K"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "L"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "M"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "N"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "O"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "P"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "Q"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "R"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "S"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "T"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "U"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "V"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "W"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "X"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "Y"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "Z"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "AA"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "AB"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "AC"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "AD"));
+		scanner.agregarDestinoAProcesar(new Destino(100, "AE"));
 
 		scanner.setearTiempoProcesamiento(TIEMPO_PROC_CAJA);
 
@@ -62,7 +75,7 @@ public class MiMain {
 					* destino.getCantidadCajas();
 		}
 		tiempoTotal += scanner.obtenerCantidadPasdasHijas() * TIEMPO_SETUP;
-//		System.out.println("TIEMPO TOTAL: " + tiempoTotal);
+		System.out.println("TIEMPO TOTAL: " + tiempoTotal);
 //		System.out.println("Cantidad de setups: " + scanner.obtenerCantidadPasdasHijas());
 
 	}
